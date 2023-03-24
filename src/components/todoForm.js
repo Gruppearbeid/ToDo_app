15be0b4a93607590594fe8ab9686435eae2e5f62
Sondre L. Ness <111089396+666949@users.noreply.github.com>
2023-03-24 23:00:58 +0100

    first test for discord webhook

--- src/components/todoForm.js
+++ src/components/todoForm.js
@@ -1,16 +1,36 @@
 import React, {useState} from 'react'
 
-function TodoForm() {
+function TodoForm(props) {
     const [input, setInput] = useState('')
 
+    const handleChange = event => {
+        setInput(event.target.value);
+    };
+
+    const handleChangeSubmit = event => {
+        // prevents the app to reload when submitting todos
+        event.preventDefault();
+        /*
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: input
+        })
+        */
+        setInput('');
+    };
+    
+
   return (
-    <form className='todo-form'>
+    <form 
+    className='todo-form' 
+    onSubmit={handleChangeSubmit}>
         <input 
         type='text' 
         placeholder='Ny oppgåve' 
         value={input}
         name='text'
         className='todo-input'
+        onChange={handleChange}
         />
         <button className='todo-button'>
             Legg til oppgåve
